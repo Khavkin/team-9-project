6ee9b348f84db2e25837580aca8387e98c1f73c9
Khavkin <hdm@ukr.net>
2023-02-19 17:23:42 +0200

    add getPopular

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './js/navigation.js';
 import LocalStorage from './js/api/local-storage-api';
+import * as FakeAPI from './js/api/fake-api';
 
 const localStorage = new LocalStorage('team-9-project');
+
+console.dir(FakeAPI.getPopularNews());
