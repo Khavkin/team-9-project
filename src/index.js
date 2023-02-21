b8e854d0c842deee20d259ed13440b1e0a9557e8
FurmanAndrew <furman.andrey.ua@gmail.com>
2023-02-21 18:06:45 +0200

    feather-feachures part1
    Add weather

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/api/nytimes-api';
 import  {Theme, refsThemeSwitcher, onCheckboxClick, changeTheme} from './js/components/theme_switcher' ;
 import LocalStorage from './js/api/local-storage-api';
 import * as FakeAPI from './js/api/fake-api';
-
+import  './js/components/weather';
 const localStorage = new LocalStorage('team-9-project');
 
 console.dir(FakeAPI.getPopularNews());
