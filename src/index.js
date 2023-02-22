dbc5d35d6b1780ff2a771ea281bb2aa324f523b6
Khavkin <hdm@ukr.net>
2023-02-23 01:56:01 +0200

    install lodash.debounce

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import debounce from 'lodash.debounce';
 import './js/navigation.js';
 import './js/api/nytimes-api';
 import LocalStorage from './js/api/local-storage-api';
-import  {Theme, refsThemeSwitcher, onCheckboxClick, changeTheme} from './js/components/theme_switcher' ;
+import {
+    Theme,
+    refsThemeSwitcher,
+    onCheckboxClick,
+    changeTheme,
+} from './js/components/theme_switcher';
 import * as FakeAPI from './js/api/fake-api';
 import './js/components/weather';
 import './js/news-cards.js';
