6b775ebd08f473e67f22a74256bd8fbf1aeabaf6
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-02-24 02:10:50 +0200

    start

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,32 +1,20 @@
 const modal = document.querySelector('.modal');
 const modalButton = document.querySelector('.menu-burger__header');
 const closeButton = document.querySelector('.close-modal');
+const themeSwitch = document.querySelector('.theme-switch');
+
+if (window.innerWidth < 767) themeSwitch.style.display = 'none';
 
 modalButton.addEventListener('click', () => {
- 
-  modal.style.top = '0';
+    modal.style.top = '0';
 });
 
 closeButton.addEventListener('click', () => {
- 
   modal.style.top = '-100%';
 });
 
-window.addEventListener('click', (event) => {
-  if (event.target === modal) {
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
     modal.style.display = 'none';
   }
 });
-
-const themeSwitch = document.querySelector('.theme-switch');
-
-if (window.innerWidth < 767) {
-  themeSwitch.style.display = 'none';
-}
-
-
-
-
-
-
-
